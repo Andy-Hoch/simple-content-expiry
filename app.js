c45acfcc5c65ad1dda1d32755352630cb8322bd4
Andy-Hoch <47834721+Andy-Hoch@users.noreply.github.com>
2022-08-30 15:38:07 +0200

    Added Files. NOW WORKING :)

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-function allExpiringElements()
+/*function allExpiringElements()
 {
   let matchingElements = [];
   let allElements = document.getElementsByTagName('*');
@@ -28,11 +28,56 @@ function hideExpiredElements()
         day = allExpiringElements[i].attributes["exDay"];
         hour = allExpiringElements[i].attributes["exHour"];
         minute = allExpiringElements[i].attributes["exMin"];
-        let expiryTime = new Date(year,month,day,hour, min);
+        let expiryTime = new Date(year,month,day,hour,min);
         
+        if (now > expiryTime) {
+            allExpiringElements[i].style.display = 'none';
+        } //Does it work? I have no idea. 
+    }
+}*/
+
+/*function allExpiringElements(attribute)
+{
+  let matchingElements = [];
+  let allElements = document.querySelectorAll('[exYear]');
+  for (let i = 0, n = allElements.length; i < n; i++)
+  {
+    if (allElements[i].getAttribute(attribute) !== null)
+    {
+      // Element exists with attribute. Add to array.
+      matchingElements.push(allElements[i]);
+    }
+  }
+  console.log(allElements);
+  return matchingElements;
+}*/
+
+let allExpiringElements = document.querySelectorAll('[exYear]')
+
+console.log(allExpiringElements);
+
+function hideExpiredElements()
+{
+    for (let i = 0, n = allExpiringElements.length; i < n; i++)
+    {
+        let now = new Date();
+        let year = now.getFullYear;
+        let month;
+        let day;
+        let hour = 0;
+        let min = 0;
+        year = allExpiringElements[i].attributes["exYear"].value;
+        console.log(year)
+        month = allExpiringElements[i].attributes["exMonth"].value - 1;
+        day = allExpiringElements[i].attributes["exDay"].value;
+        hour = allExpiringElements[i].attributes["exHour"].value;
+        minute = allExpiringElements[i].attributes["exMin"].value;
+        let expiryTime = new Date(year,month,day,hour, min);
+        console.log(expiryTime);
         if (now > expiryTime) {
             allExpiringElements[i].style.display = 'none';
         } //Does it work? I have no idea. 
     }
 }
 
+hideExpiredElements();
